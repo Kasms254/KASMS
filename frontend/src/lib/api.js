41b54f6862fccd11c099fd51e057e552801297a9
Eric Muturi Mwangi <muturiwamwanginanyawira@gmail.com>
2025-12-03 20:34:32 +0300

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -2,7 +2,7 @@
 import * as authStore from './auth'
 
 // const API_BASE = import meta.env.VITE_API_BASE || import.meta.env.VITE_API_URL;
-const API_BASE = import.meta.env.VITE_API_BASE || import.meta.env.VITE_API_URL;
+const API_BASE = import.meta.env.VITE_API_URL;
 async function request(path, { method = 'GET', body, headers = {} } = {}) {
   const url = `${API_BASE}${path}`
   const token = authStore.getToken()
