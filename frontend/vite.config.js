a1b5eb6c944d76060046dffc043a75d98d9b409e
Ericmuturimwangi <muturiwamwanginanyawira@gmail.com>
2026-01-08 19:24:40 +0300

    vite config modification.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,20 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   // Disable source maps in production to hide source code
-  //   sourcemap: false,
-  //   minify: 'esbuild', // esbuild is faster than terser
-  //   rollupOptions: {
-  //     output: {
-  //       manualChunks: {
-  //         vendor: ['react', 'react-dom', 'react-router-dom'],
-  //       },
-  //     },
-  //   },
-  // },
-  // esbuild: {
-  //   // Remove ALL console methods and debugger statements in production
-  //   drop: process.env.NODE_ENV === 'production' ? ['console', 'debugger'] : [],
-  // },
+  build: {
+    // Disable source maps in production to hide source code
+    sourcemap: false,
+    minify: 'esbuild', // esbuild is faster than terser
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+  esbuild: {
+    // Remove ALL console methods and debugger statements in production
+    drop: process.env.NODE_ENV === 'production' ? ['console', 'debugger'] : [],
+  },
 })
