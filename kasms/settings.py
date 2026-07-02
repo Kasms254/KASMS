12d8b59ad9e31725c35e5df51c82882a250ea978
BecaBakari <becabakari1@gmail.com>
2026-07-02 08:25:19 +0300

    biometric sync refcator through adding a 30 sec automatic scheduler, avoiding the admin sync

--- kasms/settings.py
+++ kasms/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
-    "core",
+    "core.apps.CoreConfig",
     # 'rest_framework.authtoken',
     "rest_framework",
     "django_filters",
